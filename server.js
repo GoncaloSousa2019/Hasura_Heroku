a32568f1c3a3fa0ec868605b020b701d0a889f6e
GoncaloSousa2019 <goncalosousa.pt@gmail.com>
2020-04-04 13:53:16 +0100

    commit 3

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const port = process.env.PORT || 8080;
 const app = express()
- 
+/*
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
+*/
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html")
+})
 app.listen(port)
 console.log("Server started "+ port)
